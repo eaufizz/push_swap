16d42f91a18ff4aa7df67866eff52c96cc9a09db
Shinohara Reo <sreo@c2r5s7.42tokyo.jp>
2024-11-10 10:42:25 +0900

    a

--- sort.c
+++ sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   sort.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: boss <boss@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: sreo <sreo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 21:39:26 by boss              #+#    #+#             */
-/*   Updated: 2024/11/10 01:42:54 by boss             ###   ########.fr       */
+/*   Updated: 2024/11/10 09:48:48 by sreo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,47 +41,13 @@ int	check_stack_b(t_list *stack_b, int digit)
 	return (0);
 }
 
-void sort_stack(t_list **stack_a, t_list **stack_b)
-{
-	int		maxbit;
-	int		digit;
-
-	digit = 0;
-	maxbit = max_bit(ft_lstsize(*stack_a) - 1);
-	while(digit <= maxbit)
-	{
-		while(check_stack_a(*stack_a, digit))
-		{
-			if (((*stack_a)->index >> digit) & 1)
-				ra(stack_a);
-			else
-				pb(stack_b, stack_a);
-		}
-		digit ++;
-		if(digit > maxbit)
-			break;
-		while(check_stack_b(*stack_b, digit))
-		{
-			if (((*stack_b)->index >> digit) & 1)
-				pa(stack_a, stack_b);
-			else
-				rb(stack_b);
-		}
-	}
-	while(*stack_b != NULL)
-		pa(stack_a, stack_b);
-}
-
-
 // void sort_stack(t_list **stack_a, t_list **stack_b)
 // {
 // 	int		maxbit;
 // 	int		digit;
-// 	int		stack_len;
 
 // 	digit = 0;
-// 	stack_len = ft_lstsize(*stack_a);
-// 	maxbit = max_bit(stack_len - 1);
+// 	maxbit = max_bit(ft_lstsize(*stack_a) - 1);
 // 	while(digit <= maxbit)
 // 	{
 // 		while(check_stack_a(*stack_a, digit))
@@ -92,10 +58,47 @@ void sort_stack(t_list **stack_a, t_list **stack_b)
 // 				pb(stack_b, stack_a);
 // 		}
 // 		digit ++;
-// 		while(*stack_b != NULL)
+// 		if(digit > maxbit)
+// 			break;
+// 		while(check_stack_b(*stack_b, digit))
 // 		{
-// 			pa(stack_a, stack_b);
-			
+// 			if (((*stack_b)->index >> digit) & 1)
+// 				pa(stack_a, stack_b);
+// 			else
+// 				rb(stack_b);
 // 		}
 // 	}
+// 	while(*stack_b != NULL)
+// 		pa(stack_a, stack_b);
 // }
+
+
+void sort_stack(t_list **stack_a, t_list **stack_b)
+{
+	int		maxbit;
+	int		digit;
+	int		stack_len;
+	int		i;
+
+	digit = 0;
+	stack_len = ft_lstsize(*stack_a);
+	maxbit = max_bit(stack_len - 1);
+	while(digit <= maxbit)
+	{
+		i = 0;
+		stack_len = ft_lstsize(*stack_a);
+		while(i < stack_len)
+		{
+			if (((*stack_a)->index >> digit) & 1)
+				ra(stack_a);
+			else
+				pb(stack_b, stack_a);
+			i++;
+		}
+		digit ++;
+		while(*stack_b != NULL)
+		{
+			pa(stack_a, stack_b);
+		}
+	}
+}
